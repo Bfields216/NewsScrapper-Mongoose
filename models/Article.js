095010c5b0fc583793ea877d9bfb85673fd136a5
Brandon Fields <brandonfields10@icloud.com>
2019-10-08 15:47:40 -0400

    looking to redo

--- models/Article.js
+++ models/Article.js
@@ -16,6 +16,14 @@ var ArticleSchema = new Schema({
     type: String,
     required: true
   },
+  summary: {
+    type: String,
+    default: 'Summary Unavailable'
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
